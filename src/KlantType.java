b1d4819bd04bac95389e946d00582fe76017b30e
Will <williamvdm@live.nl>
2023-03-30 13:44:57 +0200

    add: ability to add, change and remove klanttypes

--- src/KlantType.java
+++ src/KlantType.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class KlantType {
     private String typenaam;
     private double korting;
+    public static ArrayList<KlantType> klantTypen = new ArrayList<>();
 
     public KlantType(String typenaam, double korting) {
-        this.typenaam = typenaam;
+        this.typenaam = typenaam.toLowerCase();
         this.korting = korting;
+        if (KlantType.klantTypen.stream().noneMatch((x) -> x.typenaam.equals(typenaam.toLowerCase()))) {
+            KlantType.klantTypen.add(this);
+        }
+    }
+
+    public void deleteKlantType(String typenaam) {
+        KlantType.klantTypen.removeIf((x) -> x.typenaam.equals(typenaam.toLowerCase()));
+    }
+    public String toString() {
+        return String.format("KlantType naam=%s korting=%.2f", this.typenaam, this.korting);
     }
 
     public String getTypeNaam() {
