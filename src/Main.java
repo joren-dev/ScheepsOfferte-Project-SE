8d8dcd6f681c38b97e777c6da189a2071cc7a17c
Laurens <laatmijnueffe@gmail.com>
2023-04-03 10:47:36 +0200

    fix: view offerte

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import managers.MenuManager;
 import menus.KlantTypeMenu;
-import menus.ExampleMenu;
 import menus.ViewOfferteMenu;
 
 public class Main {
@@ -12,7 +11,6 @@ public class Main {
         // Add menu items here...
         manager.addMenu("Klant Type", new KlantTypeMenu());
         manager.addMenu("View Offerte", new ViewOfferteMenu());
-        manager.addMenu("Example optie", new ExampleMenu());
 
         // Starts the menu manager
         manager.start();
