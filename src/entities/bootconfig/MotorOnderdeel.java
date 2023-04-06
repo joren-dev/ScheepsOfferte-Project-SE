73db5e02c758250cbd3b2e092a3a0fedd270811e
Joren <contactjorenx@gmail.com>
2023-04-06 20:12:53 +0200

    add: base for bootconfig

---
+++ src/entities/bootconfig/MotorOnderdeel.java
@@ -0,0 +1,12 @@
+package entities.bootconfig;
+
+public class MotorOnderdeel extends BootConfigBase
+{
+    @Override
+    public String get_price()
+    {
+        return "";
+    }
+
+    // set_dubbele_motor(boolean)
+}
---
+++ src/entities/bootconfig/UiterlijkOnderdeel.java
@@ -0,0 +1,13 @@
+package entities.bootconfig;
+
+public class UiterlijkOnderdeel extends BootConfigBase
+{
+    @Override
+    public String get_price()
+    {
+        return "";
+    }
+
+    // set_verf
+    // ...
+}
